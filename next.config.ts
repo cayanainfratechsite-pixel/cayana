dba52909c38ef77f56c41b371ac8fc8c379d3847
cayanainfratechsite-pixel <cayanainfratechsite@gmail.com>
2026-02-02 16:29:04 +0530

    Update next.config.ts for image handling
    updated

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,45 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-
   images: {
-    domains: [
-      "cayana.s3.amazonaws.com",
-      "backend.cayana.co.in",
-      "admin.cayana.co.in"
+    remotePatterns: [
+      { hostname: "images.unsplash.com" },
+      { hostname: "artgallery.yale.edu" },
+      { hostname: "www.photocrati.com" },
+      { hostname: "145.223.23.134" },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '4000',
+        pathname: '/public/**',
+      },
+      {
+        protocol: "https",
+        hostname: "cdn-icons-png.flaticon.com",
+      },
+      {
+        protocol: 'http',
+        hostname: 'custom-images.strikinglycdn.com',
+      },
+      {
+        protocol: "https",
+        hostname: "cayana.s3.amazonaws.com",
+        pathname: "/**",
+      },
     ],
+    domains: [
+      "images.unsplash.com",
+      "media.istockphoto.com",
+      "images.ctfassets.net",
+      "example.com",
+      "artgallery.yale.edu",
+      "www.photocrati.com",
+      "145.223.23.134",
+      "api.cayana.co.in",
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
