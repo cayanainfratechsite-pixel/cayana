72c58fa1e05cad1e0a3e193779a6e5415770d6af
noorfatma <noorfatmanoor411@gmail.com>
2026-02-14 16:54:25 +0530

    fixes: pixel id update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1670784637225712&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1594412811752628&ev=PageView&noscript=1"
           />
         </noscript>
         <LayoutWrapper>
@@ -49,7 +49,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1670784637225712');
+            fbq('init', '1594412811752628');
             fbq('track', 'PageView');
           `}
         </Script>
